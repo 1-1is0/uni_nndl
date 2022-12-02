5923979208c22be78e523aa8e3a86f236153cf6b
1-1is0 <amirftma@gmail.com>
2022-12-02 23:03:10 +0330

    hw1 update

--- hw1/New folder/a.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-data=pd.read_csv("./MadaLine.csv")
-
-X1=data.iloc[:,0]
-X2=data.iloc[:,1]
-target=data.iloc[:,2]
-class1=data.loc[data["0.0"]==0,:]
-class2=data.loc[data["0.0"]==1,:]
--- hw1/q2/test.py
+++
@@ -1,6 +0,0 @@
-
-import pandas as pd
-import numpy as np
-
-
-data = pd.read_csv("Attachments\Q2_Dataset\MadaLine.csv")
